e5fe35e89bf3707102d24c9980368ab4e0cf54e0
egoing <egoing@gmail.com>
2022-08-02 14:08:33 +0900

    토픽모록을 리스트에서 생성 선택된 글이 출력되도록 개선

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+topics = [
+    {'id':1, 'title':'html', 'body':'html is ...'},
+    {'id':2, 'title':'css', 'body':'css is ...'},
+    {'id':3, 'title':'js', 'body':'js is ...'}
+]
+
 @app.route("/")
 def index():
-    return render_template("welcome.html")
+    return render_template("welcome.html", topics=topics)
 
-@app.route("/read/<id>")
+@app.route("/read/<int:id>")
 def read(id):
-    return render_template("read.html")
+    topic = None
+    for row in topics:
+        if row['id'] == id:
+            topic = row
+            break
+    return render_template("read.html", topics=topics, topic=topic)
 
